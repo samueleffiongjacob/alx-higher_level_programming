dad238d4dce3a46db6dd5984574d55601a513842
samueleffiongjacob <samueleffiong685@gmail.com>
2024-02-21 04:33:17 -0800

    oxob

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -30,7 +30,7 @@ class Student:
         new_dict = {}
         for a in attrs:
             try:
-                new_dict[a]=self.__dict__[a]
+                new_dict[a] = self.__dict__[a]
             except Exception:
                 pass
         return new_dict
